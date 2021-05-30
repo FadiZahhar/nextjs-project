194f565551acfb40dc2b02a11daafc13c531ad1a
Debian <bitnami@ip-172-26-5-238.eu-central-1.compute.internal>
2021-05-30 09:59:05 +0000

    adding the showcase with route condition to show on home page only, also implement the showcase on the layout.js file

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,25 @@
 import Head from 'next/head';
+import {useRouter} from 'next/router';
 import styles from '@/styles/Layout.module.css';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 
 export default function Layout({title,keywords,description,children}) {
+    
+    const router = useRouter()
     return (
         <div>
-        <Header />
+
         <Head>
             <title>{title}</title>
             <meta name='description' content={description} />
             <meta name='keywords' content={keywords} />
         </Head>
         
-        {children}
+        <Header/>
+        {router.pathname === '/' && <Showcase />}
+        <div className={styles.container}>{children}</div>
         <Footer />
         </div>
    )
