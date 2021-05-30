c51a6dace3efcf79c87eac24c73dd84f7692abc2
Debian <bitnami@ip-172-26-5-238.eu-central-1.compute.internal>
2021-05-30 11:25:15 +0000

    fixing the config file and adding the fetch method in the getStaticProps async method

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Layout from '@/components/Layout';
 import Link from 'next/link'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-    <h1>Upcoming Events</h1>
+      <h1>Upcoming Events</h1>
+      
     </Layout>
-    )
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
