4fec3194063c175e29eabf95f6046cf70a7ebd71
Debian <bitnami@ip-172-26-5-238.eu-central-1.compute.internal>
2021-06-01 10:44:58 +0000

    fixing the header

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 import { useContext } from 'react'
 import Link from 'next/link'
 import Search from './Search'
-import AuthContext from '@/context/AuthContext'
+
 import styles from '@/styles/Header.module.css'
 
 export default function Header() {
-  const { user, logout } = useContext(AuthContext)
+
 
   return (
     <header className={styles.header}>
@@ -25,9 +25,7 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          {user ? (
-            // If logged in
-            <>
+          
               <li>
                 <Link href='/events/add'>
                   <a>Add Event</a>
@@ -38,27 +36,6 @@ export default function Header() {
                   <a>Dashboard</a>
                 </Link>
               </li>
-              <li>
-                <button
-                  onClick={() => logout()}
-                  className='btn-secondary btn-icon'
-                >
-                  <FaSignOutAlt /> Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            // If logged out
-            <>
-              <li>
-                <Link href='/account/login'>
-                  <a className='btn-secondary btn-icon'>
-                    <FaSignInAlt /> Login
-                  </a>
-                </Link>
-              </li>
-            </>
-          )}
         </ul>
       </nav>
     </header>
